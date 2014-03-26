101e5273868dfdf1cc7923f67774d3ce3c8e8064
Alex Yao Cheng <alex@riverviewms.com>
2014-03-26 17:14:25 +0800

    Switch to SDWebImage, everybody loves SDWebImage

--- Classes/IDMPhoto.m
+++ Classes/IDMPhoto.m
@@ -8,6 +8,7 @@
 
 #import "IDMPhoto.h"
 #import "IDMPhotoBrowser.h"
+#import "SDWebImageManager.h"
 
 // Private
 @interface IDMPhoto () {
@@ -135,26 +136,26 @@ caption = _caption;
             // Load async from file
             [self performSelectorInBackground:@selector(loadImageFromFileAsync) withObject:nil];
         } else if (_photoURL) {
-            // Load async from web (using AFNetworking)
-            NSURLRequest *request = [NSURLRequest requestWithURL:_photoURL];
-            
-            AFHTTPRequestOperation *op = [[AFHTTPRequestOperation alloc] initWithRequest:request];
-            op.responseSerializer = [AFImageResponseSerializer serializer];
-
-            [op setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
-                UIImage *image = responseObject;
-                self.underlyingImage = image;
-                [self performSelectorOnMainThread:@selector(imageLoadingComplete) withObject:nil waitUntilDone:NO];
-            } failure:^(AFHTTPRequestOperation *operation, NSError *error) { }];
-            
-            [op setDownloadProgressBlock:^(NSUInteger bytesRead, long long totalBytesRead, long long totalBytesExpectedToRead) {
-                CGFloat progress = ((CGFloat)totalBytesRead)/((CGFloat)totalBytesExpectedToRead);
-                if (self.progressUpdateBlock) {
-                    self.progressUpdateBlock(progress);
-                }
-            }];
-            
-            [[NSOperationQueue mainQueue] addOperation:op];
+            __weak typeof(self)weakSelf = self;
+            SDWebImageManager *manager = [SDWebImageManager sharedManager];
+            [manager downloadWithURL:_photoURL
+                             options:kNilOptions
+                            progress:^(NSInteger receivedSize, NSInteger expectedSize) {
+                                CGFloat progress = ((CGFloat)receivedSize)/((CGFloat)expectedSize);
+                                if (weakSelf.progressUpdateBlock) {
+                                    dispatch_async(dispatch_get_main_queue(), ^{
+                                        weakSelf.progressUpdateBlock(progress);
+                                    });
+                                }
+                            }
+                           completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished) {
+                               if (image && finished) {
+                                   weakSelf.underlyingImage = image;
+                                   dispatch_async(dispatch_get_main_queue(), ^{
+                                       [weakSelf imageLoadingComplete];
+                                   });
+                               }
+                           }];
         } else {
             // Failed - no source
             self.underlyingImage = nil;
---
+++ IDMPhotoBrowser+SDWebImage.podspec
@@ -0,0 +1,20 @@
+Pod::Spec.new do |s|
+  s.name          = "IDMPhotoBrowser+SDWebImage"
+  s.version       = "1.4.0"
+  s.summary       = "Photo Browser / Viewer inspired by Facebook's and Tweetbot's with ARC support, swipe-to-dismiss, image progress and more."
+  s.description   = <<-DESC
+                    Photo Browser / Viewer inspired by Facebook's and Tweetbot's with ARC support, swipe-to-dismiss, image progress and more.
+                   DESC
+  s.homepage      = "https://github.com/kukat/IDMPhotoBrowser"
+  s.license       = { :type => 'MIT', :file => 'LICENSE.txt' }
+  s.author             = { "Alex Yao Cheng" => "kukathe@gmail.com", "Ideais Mobile" => "mobile@ideais.com.br" }
+  s.platform      = :ios, '7.0'
+  s.source        = { :git => "https://github.com/kukat/IDMPhotoBrowser.git", :tag => "1.3.2" }
+  s.source_files  = 'Classes/*.{h,m}'
+  s.resources     = 'Classes/IDMPhotoBrowser.bundle', 'Classes/IDMPBLocalizations.bundle'
+  s.frameworks    = 'MessageUI', 'QuartzCore', 'SystemConfiguration', 'MobileCoreServices', 'Security'
+  s.requires_arc  = true
+  s.dependency      'SDWebImage/Core', '~> 3.6'
+  s.dependency      'DACircularProgress', '~> 2.2.0'
+  s.dependency      'SVProgressHUD', '~> 1.0'
+end
